3d9f5ee7b3a03227296050ce42023728711ad620
c4chotu <89054821+c4chotu@users.noreply.github.com>
2026-06-23 16:00:06 +0530

    comited for port change

--- vite.config.ts
+++ vite.config.ts
@@ -41,6 +41,8 @@ export default defineConfig({
       watch: {
         ignored: ["**/routeTree.gen.ts"],
       },
+      host: true,
+      port: process.env.PORT ? parseInt(process.env.PORT) : 8080,
     },
   },
   tanstackStart: {
